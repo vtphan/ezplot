c94e5b4fe5c7dc43008fbed030a8bdb77a47e08d
vtphan <vphan@memphis.edu>
2014-12-28 17:40:53 -0600

    .

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 INSTALL_DIR="$HOME/bin"
 if hash Rscript 2>/dev/null; then
    if hash curl 2>/dev/null; then
-      SCRIPT=`which Rscript`
-      echo "script is" $SCRIPT
       if [ ! -d $INSTALL_DIR ]; then
          echo "$INSTALL_DIR does not exist.  Create $INSTALL_DIR"
          mkdir $INSTALL_DIR
       fi
-      echo "Download and install R scripts."
+      SCRIPT=`which Rscript`
+      echo "Installing..."
       cd $INSTALL_DIR
       echo "#!$SCRIPT" > $INSTALL_DIR/ezplot.R
       curl -s https://raw.githubusercontent.com/vtphan/ezplot/master/ezplot.R >> $INSTALL_DIR/ezplot.R
@@ -20,9 +19,9 @@ if hash Rscript 2>/dev/null; then
          echo "export PATH=$PATH:$INSTALL_DIR" >> $HOME/.bashrc
          source $HOME/.bashrc
       fi
-      echo "ezplot.R is now installed in $INSTALL_DIR, which is in your PATH.  Open a new terminal.  These scripts are now accessible in any directory."
+      echo "ezplot.R is installed in $INSTALL_DIR.  Try ezplot.R in a new terminal."
    else
-      echo "curl is not installed. You need to save the R scripts to $INSTALL_DIR and execute the command 'chmod +u *.R' in $INSTALL_DIR."
+      echo "curl is not installed. Try manual installation."
    fi
 else
    echo "R is not installed."
